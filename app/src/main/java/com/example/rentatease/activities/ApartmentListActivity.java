6669e96e621b26921704bde2c9f5e1c5407658f4
hardeep77 <hardeep.behniwal7@gmail.com>
2021-02-12 11:07:36 -0500

    appartment functionality

---
+++ app/src/main/java/com/example/rentatease/activities/ApartmentListActivity.java
@@ -0,0 +1,18 @@
+package com.example.rentatease.activities;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.rentatease.R;
+
+public class ApartmentListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apartment_list);
+    }
+}
